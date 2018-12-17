c7a00dcfdf467b4fc95d4f4b2d78f0c1dca245ed
Jonathan Channon <jonathan.channon@gmail.com>
2018-12-17 17:35:14 +0000

    initial

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        public static AppSettings appSettings;
+        
+        static void Main(string[] args)
+        {
+            var configBuilder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json");
+
+            var config = configBuilder.Build();
+
+            appSettings = new AppSettings();
+            config.Bind(appSettings);
+            
+            Console.WriteLine(appSettings.Foo);
+
+        }
+    }
+
+    public  class AppSettings
+    {
+        public string Foo { get; set; }
+    }
+}
